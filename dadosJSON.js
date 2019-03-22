0343d47822fcb943eae662c570a991baec3f121a
Gabriel <gabriel.ishizaki@hotmail.com>
2019-03-22 15:25:56 -0300

    aaaaaaaaaaa

--- dadosJSON.js
+++ dadosJSON.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     var xhttp = new XMLHttpRequest();
     var myJson;
+    var clickedCategory;
+    var clickedItem;
     var col1 = '';
     var col2 = '';
     var col3 = '';
@@ -9,15 +11,26 @@ $(document).ready(function(){
             myJson = JSON.parse(this.responseText);
             col1 = "<table>"
             for(var i = 0; i < myJson.categorias.length; i++){
-                col1 += "<tr'><td class='categoria'>" + myJson.categorias[i].nome + "</td></tr>"
+                col1 += "<tr class='categoria'><td>" + myJson.categorias[i].nome + "</td></tr>"
             }
             col1 += "</table>"
             $("#coluna1").html(col1);
         }
         $(".categoria").click(function(){
-            alert("clicou em " + $(this).html());
+          clickedCategory = myJson.categorias[$(this).index(".categoria")];
+          col2 = "<table>"
+          for(var j = 0; j < clickedCategory.content.length; j++) {
+              col2 += "<tr class='item'><td>" + clickedCategory.content[j].nome + "</td></tr>"
+          }
+          col2 += "</table>"
+          $("#coluna2").html(col2);
+          $(".item").click(function(){
+            clickedItem = clickedCategory.content[$(this).index(".item")];
+            alert(clickedItem.criador);
+          });
         });
+
     };
-    xhttp.open("GET","produtos.json",true);   
+    xhttp.open("GET","produtos.json",true);
     xhttp.send();
 });
